285f27500afd37648074cc6667aed60a7f5d0bf8
Ben <benjamin.main88@gmail.com>
2020-10-29 10:24:09 +0000

    Project starting files

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+let gulp = require('gulp');
+let cleanCSS = require('gulp-clean-css');
+let rename = require('gulp-rename');
+let sass = require ('gulp-sass')
+
+// SASS task
+gulp.task('sass', function () {
+	return gulp.src('./scss/styles.scss')
+		.pipe(sass())
+		.pipe(rename('styles.css'))
+		.pipe(gulp.dest('./css/'));
+});
+
+// Define a new task minify-css:
+gulp.task('minify-css', () => {
+  return gulp.src('css/styles.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('./css/'));
+});
+
+// Both gulp tasks in one
+// gulp.task('{task name}', gulp.series('{task1}', '{task2}'));
+gulp.task ('minify-sass', gulp.series('sass', 'minify-css'))
+
+//return gulp.watch('customisations.scss', gulp.series('minify-sass'));
+gulp.task('watch', function () {
+return gulp.watch('scss/**/*.scss',
+  gulp.task('minify-sass'));
+} 
+);
