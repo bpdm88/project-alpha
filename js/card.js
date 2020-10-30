490f36ec2f49d017ffb613a377f429bb01337a2d
Harry Gardiner <gardiner_harry@yahoo.com>
2020-10-30 15:26:18 +0000

    Created menShop (inc new js folder), but doesnt have footer.

---
+++ js/card.js
@@ -0,0 +1,23 @@
+((d) => {
+
+    function getRandomColor() {
+        var letters = '0123456789ABCDEF';
+        var color = '#';
+        for (var i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    }
+
+    let spansArray = [...d.getElementsByClassName("color-circle")];
+
+    spansArray.forEach(element => {
+        let color = getRandomColor();
+        element.style.backgroundColor = color;
+    })
+
+
+})(document);
+
+
+
